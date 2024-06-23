a4b0e13f881c48bda8c310b6161fede47c818a3a
cardosojl <cardosojl@outlook.com.br>
2024-06-23 17:13:20 -0300

    refactored

--- DBinit/year.ts
+++
@@ -1,7 +0,0 @@
-import yearsDB from '../src/models/Years';
-
-async function yearCreate(): Promise<void> {
-    await yearsDB.create({ year: new Date().getFullYear().toString() });
-}
-
-export default yearCreate;
